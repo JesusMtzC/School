fa522c0e460e3f1d7de7d3d2c5b56c03d52b94c6
JesusMtzC <concepcion.mtzjcm@gmail.com>
2021-04-09 20:56:08 -0500

    commit inicial

---
+++ calificaciones.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+         <div class="container">
+          <a class="navbar-brand" href="#"><img height="32px" width="32px" src="./img/esc.png" ></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-3 mb-lg-0 ">
+              <li class="nav-item ">
+                <a class="nav-link " aria-current="page" href="../BASES/index.php" >Inicio</a>
+                </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/academia.php">Academia</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/Escolar.php">Escolar</a>
+                  </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     Plantel
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="../BASES/directivos.php">Directivos</a></li>
+                    <li><a class="dropdown-item" href="../BASES/contactos.php">Contactos</a></li>
+                    <li><a class="dropdown-item" href="../BASES/anuncios.php">Anuncios</a></li>
+                    <li><a class="dropdown-item" href="../BASES/Calificaciones.php">Calificaciones</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+  </header>
+      
+        <section class="jumbotron  navbar-dark navbar-toggler"> 
+           <div class="container-xl">
+              <h1 class="display-4"> School TEC</h1>
+              <div>
+                <ul class="navbar-nav ms-auto list-group-horizontal ">
+                  <li class=><a href="https://es-la.facebook.com/"><img  src="./img/facebook.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/yout.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/insta.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/correo.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/fon.png" height="32px" width="32px" alt=""></a></li>
+               </ul>
+             </div>
+           </div>
+       </section>
+       <section>
+        <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #7dc0f0;">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"></a>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/directivos.php">Directivos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/contactos.php">Contactos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/anuncios.php">Anuncios</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/Calificaciones.php">Calificaciones</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+       </section>
+       <br><hr> 
+
+       <section class="container-sm">
+           <form action="">
+            <table class="table table-primary">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Alumno</th>
+                    <th scope="col">Matematicas</th>
+                    <th scope="col">Español</th>
+                    <th scope="col">Fisica</th>
+                    <th scope="col">Historia</th>
+                    <th scope="col">Ingles</th>
+                    <th scope="col">Social</th>
+                    <th scope="col">Deportes</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                </tbody>
+              </table>
+           </form>
+       </section>
+
+       <section class="container-sm">
+        <form action="">
+         <table class="table table-danger">
+             <thead>
+               <tr>
+                 <th scope="col">#</th>
+                 <th scope="col">First</th>
+                 <th scope="col">Last</th>
+                 <th scope="col">Handle</th>
+               </tr>
+             </thead>
+             <tbody>
+               <tr>
+                 <th scope="row">1</th>
+                 <td>Mark</td>
+                 <td>Otto</td>
+                 <td>@mdo</td>
+               </tr>
+               <tr>
+                 <th scope="row">2</th>
+                 <td>Jacob</td>
+                 <td>Thornton</td>
+                 <td>@fat</td>
+               </tr>
+               <tr>
+                 <th scope="row">3</th>
+                 <td colspan="2">Larry the Bird</td>
+                 <td>@twitter</td>
+               </tr>
+             </tbody>
+           </table>
+        </form>
+    </section>
+
+
+    <section class="container-sm">
+        <form action="">
+         <table class="table table-info">
+             <thead>
+               <tr>
+                 <th scope="col">#</th>
+                 <th scope="col">First</th>
+                 <th scope="col">Last</th>
+                 <th scope="col">Handle</th>
+               </tr>
+             </thead>
+             <tbody>
+               <tr>
+                 <th scope="row">1</th>
+                 <td>Mark</td>
+                 <td>Otto</td>
+                 <td>@mdo</td>
+               </tr>
+               <tr>
+                 <th scope="row">2</th>
+                 <td>Jacob</td>
+                 <td>Thornton</td>
+                 <td>@fat</td>
+               </tr>
+               <tr>
+                 <th scope="row">3</th>
+                 <td colspan="2">Larry the Bird</td>
+                 <td>@twitter</td>
+               </tr>
+             </tbody>
+           </table>
+        </form>
+    </section>
+    
+</body>
+</html>
---
+++ contactos.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+         <div class="container">
+          <a class="navbar-brand" href="#"><img height="32px" width="32px" src="./img/esc.png" ></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-3 mb-lg-0 ">
+              <li class="nav-item ">
+                <a class="nav-link " aria-current="page" href="../BASES/index.php" >Inicio</a>
+                </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/academia.php">Academia</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/Escolar.php">Academia</a>
+                  </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     Plantel
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="../BASES/directivos.php">Directivos</a></li>
+                    <li><a class="dropdown-item" href="../BASES/contactos.php">Contactos</a></li>
+                    <li><a class="dropdown-item" href="../BASES/anuncios.php">Anuncios</a></li>
+                    <li><a class="dropdown-item" href="../BASES/Calificaciones.php">Calificaciones</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+  </header>
+      
+        <section class="jumbotron  navbar-dark navbar-toggler"> 
+           <div class="container-xl">
+              <h1 class="display-4"> School TEC</h1>
+              <div>
+                <ul class="navbar-nav ms-auto list-group-horizontal ">
+                  <li class=><a href="https://es-la.facebook.com/"><img  src="./img/facebook.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/yout.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/insta.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/correo.png" height="32px" width="32px" alt=""></a></li>
+                  <li class=><a href="#"><img src="./img/fon.png" height="32px" width="32px" alt=""></a></li>
+               </ul>
+             </div>
+           </div>
+       </section>
+       <section>
+        <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #7dc0f0;">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"></a>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/directivos.php">Directivos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/contactos.php">Contactos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/anuncios.php">Anuncios</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="../BASES/Calificaciones.php">Calificaciones</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+       </section>
+            
+        
+       <section>
+        <div class="card-group text-center position-absolute top-50 start-25">
+          <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header">CONTACTANOS</div>
+            <div class="card-body text-primary">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card border-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body text-primary">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+       </section>
+
+
+
+
+    
+       <div>
+    <footer class="blockquote-footer " >
+        Derechos Reservados &copy; 2021<br>
+        Diseñado por:--------<br>
+        Email:
+    </footer>
+    </div>
+
+
+       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+       <script src="./js/bootstrap.min.js"></script> 
+</body>
+</html>
